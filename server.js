0e8196331fa6339ca60e2e6ab9e4abca4a5cb194
Shoaib khan <shoaibmangal@yahoo.com>
2025-12-20 23:41:44 +0500

    new express product page api route

--- server.js
+++ server.js
@@ -11,3 +11,25 @@ app.get('/news', (req, res)=>{
 
 app.listen('5000')
 
+const express = require('express');
+
+const apps = express();
+
+app.get('/', (req, res)=>{
+    res.send({status:1, msg:'cart page api'})
+})
+
+app.listen('9000')
+
+
+const express = require('express')
+
+const apple = express();
+
+apple.get('/', (req, res)=>{
+    res.send({status:1, message:'product page api'})
+})
+
+apple.listen('6000')
+
+
